b57dc775f6e9d9b7cf5879f48af913e9f059d186
Enzo Altamiranda <enzo.alt@gmail.com>
2015-11-08 13:30:22 -0300

    JSON file are now being correctly parsed. Made tests.

---
+++ src/test/java/com/POD_Final/app/JSONParserTest.java
@@ -0,0 +1,82 @@
+package com.POD_Final.app;
+
+import com.POD_Final.app.client.CustomJSONParser;
+import com.POD_Final.app.client.Movie;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Enzo on 08.11.15.
+ */
+public class JSONParserTest {
+
+    @Test
+    public void testFileIsParsedCorrectly() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File testFile = new File(classLoader.getResource("data/imdb-1.json").getFile());
+
+        CustomJSONParser parser = new CustomJSONParser(testFile);
+        ArrayList<Movie> movieList = null;
+        try {
+            movieList = parser.parseJSON();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Movie movie = movieList.get(0);
+
+        Assert.assertEquals(movie.getTitle(), "The Dark Knight Rises");
+        Assert.assertEquals(movie.getDirector(), "Christopher Nolan");
+        int votes = Integer.valueOf("1030777");
+        Assert.assertEquals(movie.getVotes(), votes);
+        float metascore = Float.valueOf("78");
+        Assert.assertEquals(movie.getMetascore(), metascore);
+        String[] actors = movie.getActors();
+        Assert.assertEquals(actors[0], "Christian Bale");
+        Assert.assertEquals(actors[1], "Gary Oldman");
+        Assert.assertEquals(actors[2], "Tom Hardy");
+        Assert.assertEquals(actors[3], "Joseph Gordon-Levitt");
+    }
+
+    @Test
+    public void fileWith40EntriesIsParsedCorrectly() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File testFile = new File(classLoader.getResource("data/imdb-40.json").getFile());
+
+        CustomJSONParser parser = new CustomJSONParser(testFile);
+        ArrayList<Movie> movieList = null;
+        try {
+            movieList = parser.parseJSON();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Movie movie = movieList.get(38);
+
+        Assert.assertEquals(movieList.size(), 39);
+        Assert.assertEquals(movie.getTitle(), "Bonanza");
+    }
+
+    @Test
+    public void fileWith200EntriesIsParsedCorrectly() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File testFile = new File(classLoader.getResource("data/imdb-200.json").getFile());
+
+        CustomJSONParser parser = new CustomJSONParser(testFile);
+        ArrayList<Movie> movieList = null;
+        try {
+            movieList = parser.parseJSON();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Movie movie = movieList.get(197);
+
+        Assert.assertEquals(movieList.size(), 198);
+        Assert.assertEquals(movie.getTitle(), "Bananas");
+
+    }
+}
