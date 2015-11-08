dd178c2b35cc4b92b845f1d25ce174cdbd85e8a4
contiver <cristianontivero@gmail.com>
2015-11-08 13:38:17 -0300

    avances con mappers y reducers

---
+++ src/main/java/com/POD_Final/app/back/Movie.java
@@ -0,0 +1,10 @@
+package main.java.com.POD_Final.app.back;
+
+public class Movie {
+    private String title;
+    /* TODO completar resto */
+
+    public Movie(String title) {
+        this.title = title;
+    }
+}
