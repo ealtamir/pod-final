4f149eceda021ac8003605e0b5794ae9b9c71791
Enzo Altamiranda <enzo.alt@gmail.com>
2015-11-16 16:50:51 -0300

    Added .jar.

--- start_hazelcast.sh
+++ start_hazelcast.sh
@@ -2,7 +2,7 @@
 
 
 echo "Setting CLASSPATH."
-export CLASSPATH=./target/classes:./hazelcast.xml:./lib/hazelcast-all-3.5.2.jar
+export CLASSPATH=./target/classes:./hazelcast.xml:./lib/hazelcast-all-3.5.2.jar:./out/artifacts/imdb_parser_jar/imdb_parser.jar
 echo ">>>> Running"
 java com.hazelcast.console.ConsoleApp
 
